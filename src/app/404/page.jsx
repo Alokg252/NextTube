c9542cb18d6fbdffd6f287823a038e63313fe0d9
Alok252 <alokg252@gmail.com>
2026-09-12 01:15:11 +0530

    page 404 changes

--- src/app/404/page.jsx
+++ src/app/404/page.jsx
@@ -5,7 +5,7 @@ export default function Custom404() {
     <div className="h-screen flex flex-col items-center justify-center">
       <h1 className="text-6xl font-bold">Data Refreshing!</h1>
       <p className="text-2xl mt-4 mb-8 text-center">
-        Our service is currently unavailable. Please try again after 2 mins. we will get up soon after refreshing our data.
+        we are currently unavailable. Please try again after some time. we will get up soon after refreshing our data.
       </p>
       <Image className="mx-auto" src={wait} alt="wait" unoptimized />
       
